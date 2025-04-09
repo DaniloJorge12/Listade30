1e52954637469ed3a366ed46c6d3f8842e6a270c
Danilo <danilojmini2@gmail.com>
2025-04-09 17:25:26 -0300

    feat(if-else): adiciona classificação etária por idade

---
+++ If_Else/classificacaoetaria.js
@@ -0,0 +1,10 @@
+let idade = 15
+
+if (idade >= 0 && idade <= 12) 
+    console.log("Você é uma criança!")
+else if (idade >= 13 && idade <= 17)
+    console.log ("Você é um adolescente!")
+else if (idade >= 18 && idade <= 59)
+    console.log ("Você é um adulto!")
+else if (idade >= 60 && idade <= 120)
+    console.log ("Você é um idoso!")
