95abbf16d0831b884caa6dc74fb70429c57f54c1
Danilo <danilojmini2@gmail.com>
2025-04-09 18:23:52 -0300

    feat(switch-case): adiciona exibição da estação do ano

---
+++ SwitchCase/estacaodoano.js
@@ -0,0 +1,16 @@
+let codigo = 2
+
+switch (codigo) {
+    case 1:
+        console.log ("Verão")
+        break
+    case 2:
+        console.log ("Outono")
+        break
+    case 3:
+        console.log ("Inverno")
+        break
+    case 4:
+        console.log ("Primavera")
+        break
+}
