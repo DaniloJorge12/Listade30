2096bef3f5e7ac17e4287b4b12f90ad4fe25fd8a
Danilo <danilojmini2@gmail.com>
2025-04-09 17:55:15 -0300

    feat(if-else): adiciona identificação do menor entre três números

---
+++ If_Else/menorde3.js
@@ -0,0 +1,10 @@
+let num1 = 80
+let num2 = 15
+let num3 = 7
+
+if (num1 < num2 && num1 < num3) 
+    console.log ("Número 1 é o menor")
+else if (num2 < num1 && num2 < num3)   
+    console.log ("Número 2 é o menor")
+else if (num3 < num1 && num3 < num2) 
+    console.log ("Número 3 é o menor")
