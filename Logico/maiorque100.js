016104c4176dbbc8b898b72bae7e034a6a39e727
Danilo <danilojmini2@gmail.com>
2025-04-09 16:58:18 -0300

    feat(operadores-logicos): adiciona operação AND com booleanos

---
+++ Logico/maiorque100.js
@@ -0,0 +1,10 @@
+let num1 = 57
+let num2 = 110
+let num3 = 492
+
+if (num1 > 100)
+    console.log ("Número 1 passa de 100!")
+else if (num2 > 100)
+    console.log ("Número 2 passa de 100!")
+else (num3 > 100)
+    console.log ("Número 3 passa de 100!")
