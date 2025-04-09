631bcd9ea01c35b06eb9c7be2b2b2ef8f4013e1a
Danilo <danilojmini2@gmail.com>
2025-04-09 17:14:37 -0300

    feat(operadores-logicos): adiciona verificação de par e maior que 10

---
+++ Logico/paregrandao.js
@@ -0,0 +1,9 @@
+let num = 16
+
+if (num % 2 == 0 && num >= 10){
+    console.log ("Número par e maior que 10")
+}else if (num % 2 == 0 && num < 10){
+    console.log ("Número par")
+}else {
+    console.log ("Número ímpar")
+}
