685443d646692be2c4c873aee0e201d6ca571e31
Danilo <danilojmini2@gmail.com>
2025-04-09 16:46:27 -0300

    feat(operadores-logicos): adiciona verificação de números positivos

---
+++ Logico/ambospositivo.js
@@ -0,0 +1,15 @@
+let num1 = 2
+let num2 = -6
+
+if (num1 >= 0 ){
+    console.log ("Número 1 é positivo")
+}else if (num1 < 0){
+    console.log ("Número 1 é negativo")
+}
+if (num2 >= 0){
+    console.log ("Número 2 é positvo")
+}else if (num2 < 0){
+    console.log ("Número 2 é negativo")
+}
+
+//Copiei do meu outro repositório do  GitHub
