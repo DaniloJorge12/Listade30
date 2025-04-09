08d89246eb5264d5e3cac1dff7120aae697a15ec
Danilo <danilojmini2@gmail.com>
2025-04-09 18:19:37 -0300

    feat(switch-case): adiciona calculadora básica com operações

---
+++ SwitchCase/calculadorabasica.js
@@ -0,0 +1,18 @@
+let operacao = '+'
+let num1 = 10
+let num2 = 20
+
+switch (operacao) {
+    case '+':
+        console.log (num1 + num2)
+        break
+    case '-':
+        console.log (num1 - num2)
+        break
+    case '*':
+        console.log (num1 * num2)
+        break
+    case '/':
+        console.log (num1 / num2)
+        break
+}
